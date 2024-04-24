bb4cca06dadf0a0dc7921f0d738528d47a2f74b3
Mateo-RENAC <mateo.renac@gmail.com>
2024-04-24 15:49:45 +0200

    Model Fini :) (pour de vrai cette fois

--- SAVSM/SAVSMapp/admin.py
+++ SAVSM/SAVSMapp/admin.py
@@ -3,28 +3,9 @@ from django.contrib.admin import AdminSite
 from .models import BatchStockObject, Object, ConsoHistoryObject
 from .models import BatchStock, ConsoHistory, SAVStock, SAVConso
 
-
-# Création d'une classe AdminSite personnalisée
-class CustomAdminSite(AdminSite):
-    site_header = 'Custom Admin'  # Entête du site personnalisé
-
-# Instanciation de la classe AdminSite personnalisée
-custom_admin_site = CustomAdminSite(name='custom_admin')
-
-# Définition de classes ModelAdmin personnalisées pour vos modèles
-class BatchStockObjectAdmin(admin.ModelAdmin):
-    pass
-
-class ObjectAdmin(admin.ModelAdmin):
-    pass
-
-class ConsoHistoryObjectAdmin(admin.ModelAdmin):
-    pass
-
-# Enregistrement de vos modèles avec les classes ModelAdmin personnalisées
-custom_admin_site.register(BatchStockObject, BatchStockObjectAdmin)
-custom_admin_site.register(Object, ObjectAdmin)
-custom_admin_site.register(ConsoHistoryObject, ConsoHistoryObjectAdmin)
+admin.site.register(BatchStockObject)
+admin.site.register(Object)
+admin.site.register(ConsoHistoryObject)
 
 admin.site.register(BatchStock)
 admin.site.register(SAVStock)

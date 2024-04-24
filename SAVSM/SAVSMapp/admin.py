002c46841dc4c9d19183c8a2278676c8eb37ea41
Mateo-RENAC <mateo.renac@gmail.com>
2024-04-24 11:37:15 +0200

    Models need a little more debugging and some change but that the idea

--- SAVSM/SAVSMapp/admin.py
+++ SAVSM/SAVSMapp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import SAVStock
-from .models import SAVConso
+from .models import Object, SAVConso, BatchStock, ConsoHistory, SAVStock
+
+
+
 # Register your models here.
 admin.site.register(SAVConso)
+
 admin.site.register(SAVStock)
+
+admin.site.register(BatchStock)
+
+admin.site.register(ConsoHistory)
+
+admin.site.register(Object)
--- SAVSM/SAVSMapp/models.py
+++ SAVSM/SAVSMapp/models.py
@@ -2,23 +2,49 @@ from django.db import models
 from django.utils import timezone
 
 
-class SAVStock(models.Model) :
-    id_Stock = models.AutoField(primary_key=True)
-    name = models.CharField(max_length = 25)
-    count = models.IntegerField(default = 0)
+class Object(models.Model):
+    id_Object = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=500, default="No description")
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.description}"
 
 
-class SAVConso (models.Model) :
-    stock = models.ForeignKey(SAVStock, on_delete=models.CASCADE)
-    count = models.IntegerField(default = 0)
-    date_Precedent_batch = models.DateField(default = timezone.now)
+class BatchStock(models.Model):
+    id_BatchStock = models.AutoField(primary_key=True)
+    id_object = models.ManyToManyField(Object)
+    BatchCount = models.IntegerField(default=0)
+    date = models.DateField(default=timezone.now)
 
     def __str__(self):
-        return self.stock.name
+        return f"{', '.join(obj.name for obj in self.id_object.all()),str(self.BatchCount)}"
 
 
+class SAVStock(models.Model):
+    id_SAVStock = models.AutoField(primary_key=True)
+    id_object = models.OneToOneField(Object, on_delete=models.CASCADE)
+    stock_Count = models.IntegerField(default=0)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.id_object.name if self.id_object else 'No Object', str(self.stock_Count)}"
+
+
+class SAVConso(models.Model):
+    id_SAVConso = models.AutoField(primary_key=True)
+    id_object = models.ForeignKey(Object, on_delete=models.CASCADE)
+    conso_Count = models.IntegerField(default=0)
+    date_Precedent_Batch = models.ForeignKey(BatchStock, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.id_object.name if self.id_object else 'No Object', self.conso_Count, self.date_Precedent_Batch}"
+
+
+class ConsoHistory(models.Model):
+    id_ConsoHistory = models.AutoField(primary_key=True)
+    id_object = models.ManyToManyField(SAVConso)
+    count_Conso = models.IntegerField(default=0)
+    date_Conso_History = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.id_object.name if self.id_object else 'No Object', self.count_Conso, self.date_Conso_History}"

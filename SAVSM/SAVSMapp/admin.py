daadce99dd28afb590513fd90e628625a73ed43e
Mateo-RENAC <mateo.renac@gmail.com>
2024-04-29 13:17:41 +0200

    Change in SAVStock Better interface in Django Admin

--- SAVSM/SAVSMapp/admin.py
+++ SAVSM/SAVSMapp/admin.py
@@ -8,7 +8,23 @@ admin.site.register(Object)
 admin.site.register(ConsoHistoryObject)
 
 admin.site.register(BatchStock)
-admin.site.register(SAVStock)
+
+
+class SAVStockAdmin(admin.ModelAdmin):
+    list_display = ('object_name', 'stock_count')
+    list_filter = ('id_object__name',)
+    search_fields = ('id_object__name',)
+
+    def object_name(self, obj):
+        return obj.id_object.name
+    object_name.short_description = 'Name'
+
+    def stock_count(self, obj):
+        return obj.stock_Count
+    stock_count.short_description = 'Count'
+
+
+admin.site.register(SAVStock, SAVStockAdmin)
 
 
 class SAVConsoAdmin(admin.ModelAdmin):
@@ -24,6 +40,7 @@ class SAVConsoAdmin(admin.ModelAdmin):
         return obj.Batch.date if obj.Batch else 'No Batch'  # Champ personnalisé pour la date de batch dans l'admin
     batch_date.short_description = 'Date'  # Titre du champ dans l'admin
 
+
 admin.site.register(SAVConso, SAVConsoAdmin)
 
 
@@ -33,5 +50,6 @@ class ConsoHistoryAdmin(admin.ModelAdmin):
     def object_counts(self, obj):
         return ", ".join([f"{conso_history_object.object.name} ({conso_history_object.count})" for conso_history_object in obj.consohistoryobject_set.all()])  # Champ personnalisé pour les objets et leurs quantités dans l'admin
     object_counts.short_description = 'Objects and Counts'  # Titre du champ dans l'admin
+
 
 admin.site.register(ConsoHistory, ConsoHistoryAdmin)

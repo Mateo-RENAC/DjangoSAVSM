a8a1ba46b2fa785aced19d6f0a5a9f0ccbeaf05a
Mateo-RENAC <mateo.renac@gmail.com>
2024-05-03 15:32:51 +0200

    Don't Work

--- SAVSM/SAVSMapp/apps.py
+++ SAVSM/SAVSMapp/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.core.signals import setting_changed
 
 
 class SavsmappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'SAVSMapp'
+
+    def ready(self):
+        import signals
+        setting_changed.connect(signals.SignalSAVUse)
+

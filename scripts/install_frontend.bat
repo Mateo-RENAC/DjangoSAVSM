dd67a6b89f5e4c85bb6166b37e7cdb1dc89caafe
Seynax <seyronax@gmail.com>
2024-05-28 14:32:09 +0200

    feature(template-improvements): frontend installation script fix

--- scripts/install_frontend.bat
+++ scripts/install_frontend.bat
@@ -1,3 +1,4 @@
+npx create-next-app@latest panel-frontend
 cd panel-frontend
 npm install @mui/material @emotion/react @emotion/styled @mui/icons-material mermaid
 npm install tailwindcss postcss autoprefixer
--- scripts/run.bat
+++ scripts/run.bat
@@ -2,7 +2,7 @@
 
 cd stock
 REM Start the frontend server (assuming it's a simple HTTP server)
-start cmd /k "cd ../panel-frontend/ && npm run dex"
+start cmd /k "cd ../panel-frontend/ && npm run dev"
 
 REM Start the Django server
 ".venv\Scripts\python" manage.py runserver

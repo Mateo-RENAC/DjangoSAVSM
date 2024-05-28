0f882390583b9b0df021e0397189e65f2788978e
Seynax <seyronax@gmail.com>
2024-05-28 14:40:45 +0200

    feature(templates-improvements: fix run.bat script

--- scripts/install_frontend.bat
+++ scripts/install_frontend.bat
@@ -2,5 +2,6 @@ npx create-next-app@latest panel-frontend
 cd panel-frontend
 npm install @mui/material @emotion/react @emotion/styled @mui/icons-material mermaid
 npm install tailwindcss postcss autoprefixer
+npm install
 npx tailwindcss init -p
 cd ..
--- scripts/run.bat
+++ scripts/run.bat
@@ -1,9 +1,11 @@
 @echo off
 
-cd stock
 REM Start the frontend server (assuming it's a simple HTTP server)
-start cmd /k "cd ../panel-frontend/ && npm run dev"
+cd panel-frontend
+start cmd /k "npm run dev"
+cd ..
 
 REM Start the Django server
-".venv\Scripts\python" manage.py runserver
+cd stock
+"../.venv\Scripts\python" manage.py runserver
 cd ..

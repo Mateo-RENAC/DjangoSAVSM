9ce2172d77a4d0e30df39295a3a512f381b461a9
Mateo-RENAC <mateo.renac@gmail.com>
2024-05-30 14:59:26 +0200

    Correction dans install_frontend.bat/sh + vérification si le projet existe déjà. Règle le bug qui bloque l'installation sur windows

--- scripts/install_frontend.bat
+++ scripts/install_frontend.bat
@@ -1,7 +1,27 @@
-npx create-next-app@latest panel-frontend
-cd panel-frontend
-npm install @mui/material @emotion/react @emotion/styled @mui/icons-material mermaid
+@echo off
+
+:: Nom du projet
+set PROJECT_NAME=panel-frontend
+
+:: Vérifie si le projet existe déjà
+if exist "%PROJECT_NAME%" (
+    echo Le projet %PROJECT_NAME% existe deja. Installation des modules...
+) else (
+    echo Création du projet %PROJECT_NAME%...
+    npx create-next-app@latest %PROJECT_NAME%
+)
+
+:: Déplacez-vous dans le répertoire du projet
+cd %PROJECT_NAME%
+
+:: Installe les modules nécessaires
+npm install @mui/material @emotion/react @emotion/styled @mui/icons-material react-mermaid2 mermaid react-chartjs-2 chart.js
 npm install tailwindcss postcss autoprefixer react-router-dom
-npm install
-npx tailwindcss init -p
-cd ..
+
+:: Vérifie si tailwind.config.js n'existe pas, initialisez Tailwind CSS
+if not exist "tailwind.config.js" (
+    npx tailwindcss init -p
+)
+
+echo Installation terminée!
+pause
--- scripts/install_frontend.sh
+++ scripts/install_frontend.sh
@@ -1,7 +1,26 @@
-npx create-next-app@latest panel-frontend
-cd panel-frontend
-npm install @mui/material @emotion/react @emotion/styled @mui/icons-material react-mermaid2 mermaid react-shartjs-2 chart.js
+#!/bin/bash
+
+# Nom du projet
+PROJECT_NAME="panel-frontend"
+
+# Vérifie si le projet existe déjà
+if [ -d "$PROJECT_NAME" ]; then
+  echo "Le projet $PROJECT_NAME existe déjà. Installation des modules..."
+else
+  echo "Création du projet $PROJECT_NAME..."
+  npx create-next-app@latest $PROJECT_NAME
+fi
+
+# Déplacez-vous dans le répertoire du projet
+cd $PROJECT_NAME
+
+# Installe les modules nécessaires
+npm install @mui/material @emotion/react @emotion/styled @mui/icons-material react-mermaid2 mermaid react-chartjs-2 chart.js
 npm install tailwindcss postcss autoprefixer react-router-dom
-npm install
-npx tailwindcss init -p
-cd ..
+
+# Si tailwind.config.js n'existe pas, initialisez Tailwind CSS
+if [ ! -f "tailwind.config.js" ]; then
+  npx tailwindcss init -p
+fi
+
+echo "Installation terminée!"
